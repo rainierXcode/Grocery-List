7080278cac3ea622e27a320879834e7e04f0a225
Rainier Sapin <rainiersapin0131@gmail.com>
2023-12-13 00:15:08 +0800

    Clear All and Delete Functionality

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
-function renameItem(list){
+function renameItem(list) {
     const inputElement = list.querySelector(".itemName");
     const inputElementEditButton = list.querySelector(".editIcon")
-    const inputElementDeleteButtodn = list.querySelector(".deleteIcon")
+    const inputElementDeleteButton = list.querySelector(".deleteIcon")
     const changeButton = list.querySelector(".change-button")
     changeButton.style.display = "block"
 
     inputElementEditButton.style.display = "none"
-    inputElementDeleteButtodn.style.display = "none"
+    inputElementDeleteButton.style.display = "none"
 
     inputElement.classList.add("canRename");
     inputElement.removeAttribute("readonly")
 }
 
+
 const listItem = document.querySelectorAll(".listItem li")
 
-listItem.forEach( li =>{
+listItem.forEach(li => {
     const editButton = li.querySelector(".editIcon");
     const changeButton = li.querySelector(".change-button")
     const inputElement = li.querySelector(".itemName");
     const inputElementEditButton = li.querySelector(".editIcon")
-    const inputElementDeleteButtodn = li.querySelector(".deleteIcon")
+    const inputElementDeleteButton = li.querySelector(".deleteIcon")
 
     let itemName = inputElement.value;
 
-    editButton.addEventListener('click', ()=>{
-       renameItem(li) 
+    inputElementEditButton.addEventListener('click', () => {
+        renameItem(li)
     })
 
-    inputElement.addEventListener('input', function(event){
+    inputElement.addEventListener('input', function (event) {
         itemName = event.target.value;
     })
-    changeButton.addEventListener('click', ()=>{
-        const inputElement = li.querySelector(".itemName");
+
+    changeButton.addEventListener('click', () => {
         inputElement.value = itemName;
         changeButton.style.display = "none"
         inputElement.classList.remove("canRename");
+        inputElement.setAttribute("readonly", true);
         inputElementEditButton.style.display = "block"
-        inputElementDeleteButtodn.style.display = "block"
+        inputElementDeleteButton.style.display = "block"
     })
+
+    inputElementDeleteButton.addEventListener('click', ()=>{
+        li.style.display = "none"
+    })
+})
+
+function clearItems(){
+    listItem.forEach( li =>{
+        li.style.display = "none"
+    })
+}
+
+const clearAll = document.querySelector(".clearItem")
+clearAll.addEventListener("click", ()=>{
+    clearItems()
 })
